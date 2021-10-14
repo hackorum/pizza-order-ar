ecc9c766aba86915f7945e119f1e75e59f8d1849
Neil Sabde <neilsabde@gmail.com>
2021-10-14 18:16:08 +0530

    Second Commit

--- js/markerHandler.js
+++ js/markerHandler.js
@@ -1,53 +1,137 @@
+let tableNumber = null;
 AFRAME.registerComponent("markerhandler", {
   init: async function () {
-
+    if (!tableNumber) {
+      this.askTableNumber();
+    }
     //get the dishes collection from firestore database
     var dishes = await this.getDishes();
 
     //markerFound event
     this.el.addEventListener("markerFound", () => {
-      var markerId = this.el.id;      
-      this.handleMarkerFound(dishes, markerId);
+      if (tableNumber) {
+        var markerId = this.el.id;
+        this.handleMarkerFound(dishes, markerId);
+      }
     });
 
     //markerLost event
     this.el.addEventListener("markerLost", () => {
       this.handleMarkerLost();
     });
-
+  },
+  askTableNumber: function () {
+    let iconURL =
+      "https://raw.githubusercontent.com/whitehatjr/menu-card-app/main/hunger.png";
+    swal({
+      title: "Welcome To The ARTEL",
+      icon: iconURL,
+      content: {
+        element: "input",
+        attributes: {
+          placeholder: "Type your table number...",
+          type: "number",
+          min: 1,
+        },
+      },
+      closeOnClickOutside: false,
+    }).then((inputValue) => {
+      tableNumber = inputValue;
+    });
+  },
+  handleOrder: function (tNum, dish) {
+    firebase
+      .firestore()
+      .collection("tables")
+      .doc(tNum)
+      .get()
+      .then((doc) => {
+        let details = doc.data();
+        if (details["current_orders"][dish.id]) {
+          details["current_orders"][dish.id]["quantity"] += 1;
+          let currentQuantity = details["current_orders"][dish.id]["quantity"];
+          details["current_orders"][dish.id]["subtotal"] =
+            currentQuantity * dish.price;
+        } else {
+          details["current_orders"][dish.id] = {
+            item: dish.dish_name,
+            price: dish.price,
+            quantity: 1,
+            subtotal: dish.price,
+          };
+        }
+        details.total_bill += dish.price;
+        firebase.firestore().collection("tables").doc(doc.id).update(details);
+      });
   },
   handleMarkerFound: function (dishes, markerId) {
-    // Changing button div visibility
-    var buttonDiv = document.getElementById("button-div");
-    buttonDiv.style.display = "flex";
+    let date = new Date();
+    let todaysDay = date.getDay();
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    // Changing Model scale to initial scale
+    let dish = dishes.filter((dish) => dish.id === markerId)[0];
 
-    var ratingButton = document.getElementById("rating-button");
-    var orderButtton = document.getElementById("order-button");
-
-    // Handling Click Events
-    ratingButton.addEventListener("click", function () {
+    if (dish.unavailable_days.includes(days[todaysDay])) {
       swal({
         icon: "warning",
-        title: "Rate Dish",
-        text: "Work In Progress"
+        title: dish.dish_name.toUpperCase(),
+        text: "This dish is not available today!!!!",
+        timer: 2500,
+        buttons: false,
       });
-    });
+    } else {
+      // Changing button div visibility
+      var buttonDiv = document.getElementById("button-div");
+      buttonDiv.style.display = "flex";
 
-    orderButtton.addEventListener("click", () => {
-      swal({
-        icon: "https://i.imgur.com/4NZ6uLY.jpg",
-        title: "Thanks For Order !",
-        text: "Your order will serve soon on your table!"
-      });
-    });
+      var ratingButton = document.getElementById("rating-button");
+      var orderButtton = document.getElementById("order-button");
 
-    // Changing Model scale to initial scale
-    var dish = dishes.filter(dish => dish.id === markerId)[0];
+      // Handling Click Events
+      if (tableNumber) {
+        ratingButton.addEventListener("click", function () {
+          swal({
+            icon: "warning",
+            title: "Rate Dish",
+            text: "Work In Progress",
+          });
+        });
 
-    var model = document.querySelector(`#model-${dish.id}`);
-    model.setAttribute("position", dish.model_geometry.position);
-    model.setAttribute("rotation", dish.model_geometry.rotation);
-    model.setAttribute("scale", dish.model_geometry.scale);
+        orderButtton.addEventListener("click", () => {
+          let tNum;
+          tableNumber <= 9
+            ? (tNum = `T0${tableNumber}`)
+            : (tNum = `T${tableNumber}`);
+          this.handleOrder(tNum, dish);
+          swal({
+            icon: "https://i.imgur.com/4NZ6uLY.jpg",
+            title: "Thanks For Order !",
+            text: "Your order will serve soon on your table!",
+          });
+        });
+      }
+
+      let model = document.querySelector(`#model-${dish.id}`);
+      model.setAttribute("position", dish.model_geometry.position);
+      model.setAttribute("rotation", dish.model_geometry.rotation);
+      model.setAttribute("scale", dish.model_geometry.scale);
+      model.setAttribute("visible", true);
+
+      let ingredientsContainer = document.querySelector(
+        `#main-plane-${dish.id}`
+      );
+      ingredientsContainer.setAttribute("visible", true);
+      let pricePlane = document.querySelector(`#price-plane-${dish.id}`);
+      pricePlane.setAttribute("visible", true);
+    }
   },
 
   handleMarkerLost: function () {
@@ -61,8 +145,8 @@ AFRAME.registerComponent("markerhandler", {
       .firestore()
       .collection("dishes")
       .get()
-      .then(snap => {
-        return snap.docs.map(doc => doc.data());
+      .then((snap) => {
+        return snap.docs.map((doc) => doc.data());
       });
-  }
+  },
 });
